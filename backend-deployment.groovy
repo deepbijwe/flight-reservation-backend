2a714f8952941944c7e649e7a1d302a698fe62c7
deep bijwe <deepbijwe90@gmail.com>
2026-07-27 13:05:50 +0530

    added full pipeline for my project

--- backend-deployment.groovy
+++ backend-deployment.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
     environment {
-        ECR_REPO = "deep/flight-backend"
+        DOCKER_REPO = "flightbackend"
         DOCKER_USER = "deepbijwe"
         CLUSTER_NAME = "cbz-cluster"
         REGION = "ap-south-1"
@@ -39,67 +39,74 @@ pipeline {
         stage('Build Docker Image') {
             steps {
                 sh '''
-                docker build -t ${ECR_REPO}:${BUILD_NUMBER} .
+                docker build -t ${DOCKER_REPO}:${BUILD_NUMBER} .
                 ''' 
             }
         }
-        // stage('Docker login'){
-        //     steps{
-        //        withCredentials([
-        //                 usernamePassword(
-        //                     credentialsId: 'docker-hub-creds',
-        //                     usernameVariable: 'DOCKER_USERNAME',
-        //                     passwordVariable: 'DOCKER_PASSWORD'
-        //                 )
-        //             ]) 
-        //             {
-        //                 sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}'
-        //             }
-        //         }
-        //     }
+        stage('Docker login'){
+            steps{
+               withCredentials([
+                        usernamePassword(
+                            credentialsId: 'Docker_Creds',
+                            usernameVariable: 'DOCKER_USERNAME',
+                            passwordVariable: 'DOCKER_PASSWORD'
+                        )
+                    ]) 
+                    {
+                        sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}'
+                    }
+                }
+            }
 
-        // stage('Docker Push') {
-        //     steps {
-        //         withCredentials([
-        //             usernamePassword(
-        //                 credentialsId: 'docker-hub-creds',
-        //                 usernameVariable: 'DOCKER_USERNAME',
-        //                 passwordVariable: 'DOCKER_PASSWORD'
-        //             )
-        //         ]) {
-        //             sh '''
-        //                 docker tag ${DOCKER_REPO}:${BUILD_NUMBER} \
-        //                 ${DOCKER_USER}/${DOCKER_REPO}:${BUILD_NUMBER}
+        stage('Docker Push') {
+            steps {
+                withCredentials([
+                    usernamePassword(
+                        credentialsId: 'Docker_Creds',
+                        usernameVariable: 'DOCKER_USERNAME',
+                        passwordVariable: 'DOCKER_PASSWORD'
+                    )
+                ]) {
+                    sh '''
+                        docker tag ${DOCKER_REPO}:${BUILD_NUMBER} \
+                        ${DOCKER_USER}/${DOCKER_REPO}:${BUILD_NUMBER}
 
-        //                 docker push ${DOCKER_USER}/${DOCKER_REPO}:${BUILD_NUMBER}
-        //             '''
-        //         }
-        //     }
-        // }
-        // stage('Image-Name-change'){
-        //         steps {
-          
-        //             sh '''
-        //              sed -i "s|mayurwagh/node-app:latest|${DOCKER_USER}/${DOCKER_REPO}:${BUILD_NUMBER}|g" k8s/deployment.yaml
-        //             '''
-        //             sh 'cat k8s/deployment.yaml'
-        //         }
-        //     }
-        // stage('Deploy to cluster'){
-        //         steps{
-        //             withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-        //                  sh '''
-        //                     aws eks update-kubeconfig --name ${CLUSTER_NAME} --region ${REGION}
+                        docker push ${DOCKER_USER}/${DOCKER_REPO}:${BUILD_NUMBER}
+                    '''
+                }
+            }
+        }
+        stage('Image-Name-change'){
+                steps {
+            
+                    sh """
+                    sed -i "s|image: .*|image: ${DOCKER_REPO}/${DOCKER_USER}:${BUILD_NUMBER}|" k8s/deployment.yaml
+                    """
+                    sh 'cat k8s/deployment.yaml'
+                }
+            }
+        stage('Deploy to cluster'){
+                steps{
+                    withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AWS_Creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                         sh '''
+                            aws eks update-kubeconfig --name ${CLUSTER_NAME} --region ${REGION}
                             
-        //                     kubectl get nodes
-        //                     kubectl apply -f k8s/deployment.yaml
-        //                     kubectl apply -f k8s/service.yaml
-        //                     kubectl get pods 
-        //                     kubectl get deployment
-        //                     kubectl get svc 
-        //                  '''
-        //             }
-        //         }
-        //    }
-    }
+                            kubectl get nodes
+                            kubectl apply -f k8s/deployment.yaml
+                            kubectl apply -f k8s/service.yaml
+                            kubectl get pods 
+                            kubectl get deployment
+                            kubectl get svc 
+                         '''
+                    }
+                }
+           }
+
+       post {
+            always {
+                cleanWs()
+            }
+        }    
+    }   
+    
 }
